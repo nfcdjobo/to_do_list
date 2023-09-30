f2e7b108af14cc3f0c269bc288f016b88b651842
nfcdjobo <nfcdjobo@gmail.com>
2023-09-30 11:10:24 +0000

    m

---
+++ login.js
@@ -0,0 +1,36 @@
+const dataLocal = localStorage.USERS ?JSON.parse(localStorage.USERS) : [];
+console.log(dataLocal)
+
+if(localStorage.SESSIONUSER){
+    window.location.href = "./profile.html";
+}
+const message =document.getElementById("message")
+
+if(!dataLocal.length) {
+    message.classList.add("error");
+    message.textContent = "Vous n'avez pas de compte, cherchez à vous inscrire !"
+    setTimeout(() => {
+        window.location.href = "./register.html";
+    }, 2000);
+    
+}
+console.log(document.querySelector("form"))
+document.querySelector("form").addEventListener("submit", event=>{
+    event.preventDefault();
+    if(dataLocal.find(user=>user.email === event.target.email.value && event.target.password.value === user.password)){
+        const data = {
+            email: event.target.email.value,
+            password: event.target.password.value,
+            durre: new Date()+6*60*60*60*1000
+        };
+        localStorage.setItem("SESSIONUSER", JSON.stringify(data));
+        message.classList.add("success")
+        message.textContent = "Connexion effectuée !"
+        setTimeout(() => {
+            window.location.href = "./profile";
+        }, 2000);
+    }else{
+        message.classList.add("error")
+        message.textContent = "Email ou mot de passe incorrect !"
+    }
+})
